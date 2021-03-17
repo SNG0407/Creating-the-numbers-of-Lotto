51e4c09b9d196f45d717228cbf0d1d8a5cc95522
kim <ksc0407@khu.ac.kr>
2021-03-17 19:35:18 +0900

    초기 커밋.

---
+++ Creating the numbers of Lotto/Creating the numbers of Lotto.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <time.h>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+
+    system("cls");
+
+    srand((unsigned int)time(0));
+
+    int Lotto[45] = {};
+    // 1~45 까지의 숫자를 차례로 넣어준다. 
+    for (int i = 0; i < 45; i++) {
+        Lotto[i] = i + 1;
+        cout << Lotto[i];
+    }
+
+    return 0;
+}
