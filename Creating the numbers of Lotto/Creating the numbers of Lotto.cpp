8620e265976d8c703469cb127614102fb3923754
kim <ksc0407@khu.ac.kr>
2021-03-17 20:07:40 +0900

    로또 당첨 번호 추첨 프로그램 완성

--- Creating the numbers of Lotto/Creating the numbers of Lotto.cpp
+++ Creating the numbers of Lotto/Creating the numbers of Lotto.cpp
@@ -1,11 +1,8 @@
 ﻿#include <iostream>
 #include <time.h>
-
+using namespace std;
 int main()
 {
-    std::cout << "Hello World!\n";
-
-    system("cls");
 
     srand((unsigned int)time(0));
 
@@ -13,8 +10,41 @@ int main()
     // 1~45 까지의 숫자를 차례로 넣어준다. 
     for (int i = 0; i < 45; i++) {
         Lotto[i] = i + 1;
-        cout << Lotto[i];
     }
+    cout << "아래의 숫자중 6가지 숫자와 보너스 숫자 1가지를 랜덤하게 알려드립니다." << endl;
+    for (int i = 0; i < 45; i++) {
 
+        if (Lotto[i] < 10) {
+            cout << "0" << Lotto[i] << " ";
+        }
+        else { cout << Lotto[i] << " "; }
+        if ((i + 1) % 5 == 0) { cout << endl; }
+    }
+    cout << "\t";
+    cout << "\n";
+
+    //suffles
+    int emptyShit, index1, index2;
+    for (int i = 0; i < 100; i++) {
+        index1 = rand() % 45;
+        index2 = rand() % 45;
+        emptyShit = Lotto[index1];
+        Lotto[index1] = Lotto[index2];
+        Lotto[index2] = emptyShit;
+    }
+    cout << "당첨 번호 7가지 : ";
+    for (int i = 0; i < 6; i++) {
+        
+        if (Lotto[i] < 10) {
+            cout <<"0"<<Lotto[i]<<" ";
+        }
+        else { cout << Lotto[i] << " "; }
+        
+    }
+    cout << "\t";
+    cout << "보너스 : " << Lotto[6];
+    
+    cout << "\n";
+    
     return 0;
 }
